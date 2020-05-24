2f800c0f4e0170df43de7d533a91c69cae1bc532
homerr <19776252+homerr@users.noreply.github.com>
2020-05-24 22:20:09 +0100

    importing

---
+++ src/components/HelloFace.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="facecake" class="container">
+        <p class="bg-primary text-white">Muggins face</p>
+        <p class="bg-warning text-white">{{ knob }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Facecake",
+    props: {
+        knob: String
+    }
+};
+</script>
+
+<style>
+    
+</style>
---
+++ src/views/Noob.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="noobhome">
+        <h1>Fookin ell</h1>
+        <Facecake knob="ya mug"/>
+    </div>
+</template>
+
+<script>
+import Facecake from '@/components/HelloFace.vue'
+
+export default {
+    name: 'Noobhome',
+    components: {
+        Facecake
+    }
+};
+</script>
+
+<style>
+
+</style>
